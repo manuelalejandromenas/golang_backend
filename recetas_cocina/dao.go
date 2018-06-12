930e56976543f92657e4f687143405ec871a4264
Manuel Alejandro <manuel.mena@correounivalle.edu.co>
2018-06-12 22:47:31 +0000

    JSON: New changes

--- recetas_cocina/dao.go
+++ recetas_cocina/dao.go
@@ -193,33 +193,33 @@ func pruebasDAO() {
 }
 
 type RecetaJSON struct {
-	nombre string
-	descripcion string
+	nombre       string
+	descripcion  string
 	ingredientes string
-	pasos string
+	pasos        string
 }
 
 func crearReceta(json string) {
 	var json_bytes []byte = []byte(json)
 	var receta_json RecetaJSON
 	err := json.Unmarshal(json_bytes, &receta_json)
-	if ( err != nil ) {
+	if err != nil {
 		panic(err)
 	}
-	receta := Receta{-1,receta_json.nombre,receta_json.descripcion,receta_json.ingredientes,receta_json.pasos}
-	
+	receta := Receta{-1, receta_json.nombre, receta_json.descripcion, receta_json.ingredientes, receta_json.pasos}
+
 	receta.crearEnBD()
 }
 
 func consultarReceta(id_receta int) string {
 	var json string
-	receta := Receta{id_receta,"","","",""}
-	
-	bool existe = receta.existeEnBD()
+	receta := Receta{id_receta, "", "", "", ""}
+
+	existe := receta.existeEnBD()
 	if existe {
 		receta.consultarEnBD()
 		json_bytes, err := json.Marshall(receta)
-		if ( err != nil ) {
+		if err != nil {
 			panic(err)
 		}
 		json = string(json_bytes[:])
@@ -233,42 +233,42 @@ func listarRecetasEnJson() []string {
 	var recetas_en_json []string
 	for i, receta := range recetas {
 		json_bytes, err := json.Marshall(receta)
-		if ( err != nil ) {
+		if err != nil {
 			panic(err)
 		}
 		json = string(json_bytes[:])
 		append(recetas_en_json, json)
 	}
-	
+
 	return recetas_en_json
 }
 
 func modificarReceta(id_receta int, json string) bool {
-	
-	receta := Receta{id_receta,"","","",""}
-	bool existe = receta.existeEnBD()
+
+	receta := Receta{id_receta, "", "", "", ""}
+	existe := receta.existeEnBD()
 	if existe {
 		var json_bytes []byte = []byte(json)
 		var receta_json RecetaJSON
 		err := json.Unmarshal(json_bytes, &receta_json)
-		if ( err != nil ) {
+		if err != nil {
 			panic(err)
 		}
-		receta := Receta{id_receta,receta_json.nombre,receta_json.descripcion,receta_json.ingredientes,receta_json.pasos}
+		receta := Receta{id_receta, receta_json.nombre, receta_json.descripcion, receta_json.ingredientes, receta_json.pasos}
 		receta.actualizarEnBD()
 	}
 	return existe
-	
+
 }
 
 func eliminarReceta(id_receta int) bool {
 	var json string
-	receta := Receta{id_receta,"","","",""}
-	
-	bool existe = receta.existeEnBD()
+	receta := Receta{id_receta, "", "", "", ""}
+
+	existe := receta.existeEnBD()
 	if existe {
 		receta.eliminarEnBD()
-	} 
+	}
 	return existe
 }
 
@@ -284,15 +284,13 @@ func pruebasJSON() {
 	fmt.Printf(consultarReceta(2))
 	fmt.Printf("\nMODIFICAR\n")
 	se_pudo_modificar := modificarReceta(3, `{"nombre":"PRUEBA 1","descripcion":"PRUEBA 1","ingredientes":"PRUEBA 1", "pasos":"PRUEBA 1"}`)
-	
+
 	fmt.Printf("\nLISTADO\n")
 	var recetas2 []string = listarRecetasEnJson()
 	for i, v := range recetas2 {
 		fmt.Printf("%v\n", v)
 	}
-	
-	
-	
+
 }
 func main() {
 	//pruebasDAO()
